31d73fb6b69f03d38daac1642172505e288612d5
Narmod <nimrodbenhamo@gmail.com>
2024-05-28 22:30:37 +0300

    tried to add tree view, still dosent work

--- GUI/Pages/MainGUI.xaml.cs
+++ GUI/Pages/MainGUI.xaml.cs
@@ -181,8 +181,7 @@ namespace RemoteSync
                 }
             }
         }
-        public async Task<List<(int, string)>> GetProcesscesFromServer(string ip) => (await SendRequest(new Packet(RequestType.Get, ""), ip)).GetContentAsString().Split('#').Select(x => x.Split('|')).Select(x => (int.Parse(x[0]), x[1])).ToList();
-        public async Task<List<(int, string, string)>> GetProcesscesFromServerNew(string ip) 
+        public async Task<List<(int, string, string)>> GetProcesscesFromServer(string ip) 
         {
             var requset = await SendRequest(new Packet(RequestType.Get, ""), ip);
             var content = requset.GetContentAsString();
@@ -297,8 +296,8 @@ namespace RemoteSync
                             }
                         }
 
-                        var newProcesses = await GetProcesscesFromServerNew(ip);
-                        Dispatcher.Invoke(() => UpdateProcessListNew(newProcesses, clientListBox));
+                        var newProcesses = await GetProcesscesFromServer(ip);
+                        Dispatcher.Invoke(() => UpdateProcessList(newProcesses, clientListBox));
                     }
                     catch (Exception e)
                     {
@@ -310,68 +309,8 @@ namespace RemoteSync
                 }
             }
         }
-
         private ObservableCollection<ListItem> items = new ObservableCollection<ListItem>();
-        private void UpdateProcessList(List<(int, string)> newProcesses, ListBox clientListBox)
-        {
-            var ProcessesListItem = new List<ListItem>();
-            // Convert newProcesses to ListItem objects
-            foreach (var process in newProcesses)
-            {
-                ListItem current = new ListItem { Name = process.Item2, ID = process.Item1 };
-                ProcessesListItem.Add(current);
-            }
-
-            ListBox currentListBox = clientListBox;
-
-            // Remove duplicates from the ListBox
-            foreach (var newItem in ProcessesListItem)
-            {
-                bool alreadyExists = false;
-                foreach (var listBoxItem in currentListBox.Items)
-                {
-                    var existingItem = listBoxItem as ListItem;
-                    if (existingItem != null && existingItem.ID == newItem.ID && existingItem.Name == newItem.Name)
-                    {
-                        alreadyExists = true;
-                        break;
-                    }
-                }
-                if (!alreadyExists)
-                {
-                    // Add new item to ListBox only if it contains the search term
-                    if (search == "" || newItem.Name.Contains(search))
-                    {
-                        items.Add(newItem);
-                        currentListBox.Items.Add(newItem);
-                    }
-                }
-            }
-
-            // Remove items that do not exist in newProcesses
-            for (int i = currentListBox.Items.Count - 1; i >= 0; i--)
-            {
-                var item = currentListBox.Items[i] as ListItem;
-                if (item != null && !ProcessesListItem.Any(p => p.ID == item.ID && p.Name == item.Name))
-                {
-                    currentListBox.Items.RemoveAt(i);
-                }
-            }
-            // Remove items that do not contain the search term
-            if (search != "")
-            {
-                for (int i = currentListBox.Items.Count - 1; i >= 0; i--)
-                {
-                    var item = currentListBox.Items[i] as ListItem;
-                    if (item != null && !item.Name.Contains(search))
-                    {
-                        currentListBox.Items.RemoveAt(i);
-                    }
-                }
-            }
-            
-        }
-        private void UpdateProcessListNew(List<(int, string, string)> newProcesses, ListBox clientListBox)
+        private void UpdateProcessList(List<(int, string, string)> newProcesses, ListBox clientListBox)
         {
             var ProcessesListItem = new List<ListItem>();
             // Convert newProcesses to ListItem objects
@@ -430,70 +369,178 @@ namespace RemoteSync
             }
 
         }
+
+
+        private async Task RefreshScreenFromServerNew(string username)
+        {
+            var serverClientList = await MongoDBfunctions.GetAllClientsAsync(username);
+            bool exist = false;
+
+            if (serverClientList != null)
+            {
+                if (clientsNumber == 0)
+                {
+                    //client list is empty, add all of the client from mongo
+                    foreach (var tuple in serverClientList)
+                    {
+                        currentClientList.Add(tuple);
+                        clientsNumber++;
+
+                        // Add here the new tab for the client
+                        TabItem newTabItem = new TabItem
+                        {
+                            Header = tuple.Item1.ToString()
+                        };
+
+                        // Replace ListBox with ListView
+                        ListView listView = new ListView
+                        {
+                            Name = tuple.Item1.ToString() + "_listview",
+                            Height = 600
+                        };
+
+                        // Set the GridView as the View for the ListView
+                        listView.View = ListViewGrid();
+
+                        newTabItem.Content = listView;
+                        listView.SelectionChanged += MainListBox_SelectionChanged; // Enables the usage of the MainListBox_SelectionChanged function                        
+                        listView.ItemTemplate = ListViewDataTemplate();
+
+                        CmpTabs.Items.Add(newTabItem);
+                    }
+                }
+                else
+                {
+                    foreach (var tuple in serverClientList)
+                    {
+                        foreach (var client in currentClientList)
+                        {
+                            if (client.Item2 == tuple.Item2)
+                            {
+                                exist = true;
+                                break;
+                            }
+                        }
+                        if (exist == false)
+                        {
+                            //the current client is not inside the tabs, so we will add it
+                            currentClientList.Add(tuple);
+                            clientsNumber++;
+
+                            // Add here the new tab for the client
+                            TabItem newTabItem = new TabItem
+                            {
+                                Header = tuple.Item1.ToString()
+                            };
+
+                            // Replace ListBox with ListView
+                            ListView listView = new ListView
+                            {
+                                Name = tuple.Item1.ToString() + "_listview",
+                                Height = 600
+                            };
+
+                            // Set the GridView as the View for the ListView
+                            listView.View = ListViewGrid();
+
+                            newTabItem.Content = listView;
+                            listView.SelectionChanged += MainListBox_SelectionChanged; // Enables the usage of the MainListBox_SelectionChanged function                        
+                            listView.ItemTemplate = ListViewDataTemplate();
+
+                            CmpTabs.Items.Add(newTabItem);
+                        }
+                    }
+                }
+            }
+            if (currentClientList != null)
+            {
+                foreach (var client in currentClientList.ToList())
+                {
+                    var name = client.Item1;
+                    var ip = client.Item2;
+                    var id = client.Item3;
+
+                    ListView clientListView = new ListView();
+
+                    try //try to update the process list for each client, if falied, client has disconnected
+                    {
+                        foreach (var item in ComputerTabs.Items)
+                        {
+                            var tab = item as TabItem;
+                            if (tab != null)
+                            {
+                                if (tab.Header != null && tab.Header.ToString() == name)
+                                {
+                                    clientListView = tab.Content as ListView;
+                                }
+                            }
+                        }
+
+                        var newProcesses = await GetProcesscesFromServer(ip);
+                        Dispatcher.Invoke(() => UpdateProcessListNew(newProcesses, clientListView));
+                    }
+                    catch (Exception e)
+                    {
+                        currentClientList.Remove(client);
+                        RemoveCurrentTab(name);
+                        clientsNumber--;
+                        await MongoDBfunctions.RemoveDisconnectedClientAsync(technicianUsername, ip);
+                    }
+                }
+            }
+        }
+        private void UpdateProcessListNew(List<(int, string, string)> newProcesses, ListView clientListView)
+        {
+            var ProcessesListItem = new List<ProcessItem>();
+
+            // Convert newProcesses to ProcessItem objects
+            foreach (var process in newProcesses)
+            {
+                ProcessItem current = new ProcessItem { ID = process.Item1, Name = process.Item2, CPU = process.Item3 };
+                ProcessesListItem.Add(current);
+            }
+
+            // Dictionary to store unique process items by ID
+            var processDictionary = new Dictionary<int, ProcessItem>();
+
+            // Convert newProcesses to ProcessItem objects and keep only the last occurrence for each ID
+            foreach (var process in newProcesses)
+            {
+                var current = new ProcessItem { ID = process.Item1, Name = process.Item2, CPU = process.Item3 };
+
+                // Replace existing item if there is a duplicate ID
+                processDictionary[current.ID] = current;
+            }
+
+            ListView currentListView = clientListView;
+
+            // Clear the ListView before updating with unique process items
+            currentListView.Items.Clear();
+
+            // Update ListView with unique process items
+            foreach (var processItem in processDictionary.Values)
+            {
+                currentListView.Items.Add(processItem);
+            }
+
+            // Remove items that do not exist in newProcesses
+            for (int i = currentListView.Items.Count - 1; i >= 0; i--)
+            {
+                var item = currentListView.Items[i] as ProcessItem;
+                if (item != null && !processDictionary.Any(p => p.Value.ID == item.ID))
+                {
+                    currentListView.Items.RemoveAt(i);
+                }
+            }
+        }
+
+
         //refresh the clients
         public static TabControl CmpTabs;
         private void ComputerTabs_Loaded(object sender, RoutedEventArgs e)
         {
             CmpTabs = (sender as TabControl);
         }
-
-        public static async Task RefreshClientsAsync(string username)
-        {
-            var serverClientList = await MongoDBfunctions.GetAllClientsAsync(username);
-            bool exist = false;
-
-            if (clientsNumber == 0)
-            {
-                //client list is empty, add all of the client from mongo
-                foreach(var tuple in serverClientList)
-                {
-                    currentClientList.Add(tuple);
-                    clientsNumber++;
-
-                    //add here the new tab for the client
-                    TabItem newTabItem = new TabItem
-                    {
-                        Header = tuple.Item1.ToString()                        
-                    };
-                    ListBox listbox = new ListBox();
-                    listbox.Name = tuple.Item1.ToString()+"_listbox";
-                    listbox.Height = 600;
-
-                    newTabItem.Content = listbox;
-                    CmpTabs.Items.Add(newTabItem);
-
-                }
-            }
-            else
-            {
-                foreach (var tuple in serverClientList)
-                {
-                    foreach (var client in currentClientList)
-                    {
-                        if(client.Item2 == tuple.Item2)
-                        {
-                            exist = true;
-                            break;
-                        }
-                    }
-                    if(exist == false)
-                    {
-                        //the current client is not inside the tabs, so we will add it
-                        currentClientList.Add(tuple);
-                        clientsNumber++;
-                        //add here the new tab for the client
-                        TabItem newTabItem = new TabItem
-                        {
-                            Header = tuple.Item1,
-                            Name = tuple.Item2
-                        };
-                        CmpTabs.Items.Add(newTabItem);
-
-                    }
-                }
-            }                      
-        }
-
 
         //setting timer for refresh
         private DispatcherTimer timer;
@@ -503,7 +550,7 @@ namespace RemoteSync
             timer = new DispatcherTimer();
             timer.Interval = TimeSpan.FromMilliseconds(2000); // Set the interval to 1000 milliseconds = 1 second           
             
-            timer.Tick += async (sender, e) => await RefreshScreenFromServer(technicianUsername);
+            timer.Tick += async (sender, e) => await RefreshScreenFromServerNew(technicianUsername);
 
             // Start the timer
             timer.Start();
@@ -597,6 +644,55 @@ namespace RemoteSync
             // return DataTemplate
             return dataTemplate;
         }
+        public DataTemplate ListViewDataTemplate()
+        {
+            DataTemplate dataTemplate = new DataTemplate();
+            FrameworkElementFactory stackPanelFactory = new FrameworkElementFactory(typeof(StackPanel));
+            stackPanelFactory.SetValue(StackPanel.OrientationProperty, Orientation.Horizontal);
+
+            FrameworkElementFactory nameTextBlock = new FrameworkElementFactory(typeof(TextBlock));
+            nameTextBlock.SetBinding(TextBlock.TextProperty, new System.Windows.Data.Binding("Name"));
+            nameTextBlock.SetValue(TextBlock.MarginProperty, new Thickness(5));
+            nameTextBlock.SetValue(TextBlock.WidthProperty, 100.0);
+            stackPanelFactory.AppendChild(nameTextBlock);
+
+            FrameworkElementFactory idTextBlock = new FrameworkElementFactory(typeof(TextBlock));
+            idTextBlock.SetBinding(TextBlock.TextProperty, new System.Windows.Data.Binding("ID"));
+            idTextBlock.SetValue(TextBlock.MarginProperty, new Thickness(5));
+            idTextBlock.SetValue(TextBlock.WidthProperty, 50.0);
+            stackPanelFactory.AppendChild(idTextBlock);
+
+            FrameworkElementFactory cpuTextBlock = new FrameworkElementFactory(typeof(TextBlock));
+            cpuTextBlock.SetBinding(TextBlock.TextProperty, new System.Windows.Data.Binding("CPU"));
+            cpuTextBlock.SetValue(TextBlock.MarginProperty, new Thickness(5));
+            cpuTextBlock.SetValue(TextBlock.WidthProperty, 100.0);
+            stackPanelFactory.AppendChild(cpuTextBlock);
+
+            dataTemplate.VisualTree = stackPanelFactory;
+            return dataTemplate;
+        }
+        public GridView ListViewGrid()
+        {
+            // Create and configure the GridView
+            GridView gridView = new GridView();
+            gridView.Columns.Add(new GridViewColumn
+            {
+                Header = "Name",
+                DisplayMemberBinding = new System.Windows.Data.Binding("Name")
+            }); //Name 
+            gridView.Columns.Add(new GridViewColumn
+            {
+                Header = "Id",
+                DisplayMemberBinding = new System.Windows.Data.Binding("Id")
+            }); //Id
+            gridView.Columns.Add(new GridViewColumn
+            {
+                Header = "CPU",
+                DisplayMemberBinding = new System.Windows.Data.Binding("CPU")
+            }); //CPU
+
+            return gridView;
+        }
     }
     public class ListItem
     {
@@ -604,5 +700,16 @@ namespace RemoteSync
         public int ID { get; set; }
         public string CPU { get; set; }
     }
+    public class ProcessItem
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string CPU { get; set; }
+        public ObservableCollection<ProcessItem> Children { get; set; } // To store child items
 
+        public ProcessItem()
+        {
+            Children = new ObservableCollection<ProcessItem>();
+        }
+    }
 }
